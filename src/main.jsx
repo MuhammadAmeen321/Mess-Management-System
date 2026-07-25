c7fb3049e9089076c2c5db1e0452e23853054926
MuhammadAmeen321 <alirazajunejo38@gmail.com>
2026-07-25 23:35:15 +0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MessManagementApp from './MessManagementApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MessManagementApp />
+  </React.StrictMode>,
+)
